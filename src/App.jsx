e9573358e8488eccc6324dcbb21389e96bc3b029
dev3-bildup <dev3-bildup@gmail.com>
2025-07-03 16:25:58 +0100

    components

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import viteLogo from '/vite.svg'
 import './index.css'
 import './App.css'
 import Header from './components/header'
+import Footer from './components/footer'
+import Homepage from './pages/homepage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header/>
+     <Homepage/>
     
     </>
   )
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,12 +14,11 @@ setIsMenuOpen(!isMenuOpen);
 return (
     <header className="w-full bg-[#fffff] text-[#161616] h-16 flex items-center shadow-md fixed top-0 left-0 z-50">
         <div className="container mx-auto flex items-center justify-between px-4 h-full">
- 
+
             <div className="flex items-center bg-red">
                 <img src="/logo.png" alt="Bildup Logo" width={24} height={24} className="mr-2" />
             </div>
 
-         
             <nav className="hidden md:flex gap-10 items-center h-full">
                 <a href="#" className="hover:underline">Home</a>
                 <a href="#" className="hover:underline">Learn how Bildup AI empowers everyone</a>
@@ -27,14 +26,12 @@ return (
                 <a href="#" className="hover:underline">Contact us</a>
             </nav>
 
-      
             <div className="hidden md:block">
                 <button className="bg-[#0071E3] text-white px-4 py-2 rounded-[32px] hover:bg-[#222] transition">Get Started</button>
             </div>
 
-    
             <button
-                className=" md:hidden items-center"
+                className="md:hidden items-center"
                 onClick={toggleMenu}
                 aria-label={isMenuOpen ? "Close menu" : "Open menu"}
             >
@@ -42,9 +39,8 @@ return (
             </button>
         </div>
 
-   
         {isMenuOpen && (
-            <div className="fixed inset-0 bg-white z-40 flex flex-col items-center justify-center md:hidden transition-all">
+            <div className="fixed inset-0 bg-white z-40 flex flex-col items-start md:hidden transition-all px-6 py-8">
                 <button
                     className="absolute top-4 right-4"
                     onClick={toggleMenu}
@@ -52,7 +48,7 @@ return (
                 >
                     <X size={32} />
                 </button>
-                <nav className="flex start font-medium flex-col gap-6 text-lg">
+                <nav className="flex font-medium flex-col mt-4 gap-6 text-lg w-full items-start">
                     <a href="#" className="hover:underline" onClick={toggleMenu}>Home</a>
                     <a href="#" className="hover:underline" onClick={toggleMenu}>Get started with Bildup AI</a>
                     <a href="#" className="hover:underline" onClick={toggleMenu}>Learn how Bildup AI empowers everyone</a>
